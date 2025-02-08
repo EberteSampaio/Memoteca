3739e16368bdba11b7db45e52d263ef9083505ba
EberteSampaio <ebertechavessampaiojr@gmail.com>
2025-02-08 12:45:38 -0300

    feat: change static values to ngModel

--- src/app/components/thought/create/create.component.ts
+++ src/app/components/thought/create/create.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit{
 
+  thought ={
+    id: '1',
+    content: 'Aprendendo Angular',
+    author: 'Eberte C. S. Junior',
+    model: 'model1'
+  };
+
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void
+  {
+
+  }
+
+  createThought(): void
+  {
+    alert("Created Thought");
+  }
+
+
+  cancelThought(): void
+  {
+    alert("Canceled Thought");
+  }
 }
